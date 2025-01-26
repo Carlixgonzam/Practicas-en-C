8cbbb33b7488a01f588725be7de108fbe7c5673b
Carla Gonzalez <carlafernanda006@gmail.com>
2025-01-26 15:55:05 -0500

    Verifica si es mayor de edad

--- Programa1.c
+++ Programa1.c
@@ -13,6 +13,7 @@ int main(){
     scanf("%d", &num2);
     resultado = a*(num1 + num2) + b*(b+num2);
     printf("El resultado es: %d\n", resultado);
+    funcion();
     return 0; //Se utiliza para indicar que el programa se ejecuto correctamente
 
 }
@@ -27,3 +28,23 @@ int main(){
 //Se utiliza el comando gcc -g Programa1.c -o Programa1 para compilar el programa con informacion de depuracion
 //Se utiliza el comando gdb Programa1 para depurar el programa
 
+/*Toma de desiciones (if)  operadores de Igualdad
+    == X ES IGUAL QUE Y
+    != x es diferente de y*/
+/*Operadores de relacion
+    > x en mayor que y
+    < x es menor que y
+    >= x es mayor o igual que y
+    <= x es menor o igual que y*/
+
+int funcion(){
+    int edad;
+    printf("Ingrese su edad\n");
+    scanf("%d", &edad);
+    if(edad >= 18){
+        printf("Eres mayor de edad\n");
+    }else{
+        printf("Eres menor de edad\n");
+    }
+    return 0;
+}
